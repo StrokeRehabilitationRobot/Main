0e88a8714f9d0bb02b0447f225fb57171735dfb3
ngoldfarb <nagoldfarb@wpi.edu>
2017-11-13 23:03:30 -0500

    added functions to get state

--- Robot.py
+++ Robot.py
@@ -2,4 +2,20 @@
 
 class Robot(object):
     def __init__(self):
-        pass
+        self._inertia = [[1, 1, 1], [1, 1, 1], [1, 1, 1]] # Ix Iy Iz
+        self._m = [1, 1, 1]
+        self._lengths = [1, 1, 1]
+        self._centroid = [ 0.5,0.5,0.5]
+        self.q = [0,0,0]
+        self.qd = [ 0,0,0]
+        self.qdd = [0, 0, 0]
+
+    def update_q(self,q):
+        self.q = q
+
+    def update_qd(self,qd):
+        self.qd = qd
+
+    def update_qdd(self,qdd):
+        self.qdd = qdd
+
