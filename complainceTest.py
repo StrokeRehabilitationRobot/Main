b79e6b40f0efe83e1618a3329c7ecf483f58c23d
ngoldfarb <nagoldfarb@wpi.edu>
2017-12-05 15:35:30 -0500

    two links work

--- complainceTest.py
+++ complainceTest.py
@@ -6,7 +6,7 @@ from Dynamics import Dynamics
 from Haptic_Controller import GravityCompensationController
 import time
 import numpy as np
-
+import time
 robot = Robot.Robot()
 ploter = PlotArm.PlotArm()
 
@@ -21,36 +21,29 @@ packet = 15*[0,0,0]
 sinWaveInc = 10;
 sin_range = 400;
 count = 0
-Kv = np.matrix([ [1,0,0],[0,1,0],[0,0,1]])
-Kl = np.matrix([ [-1,0,0],[0,-1,0],[0,0,1]])
+Kv = np.matrix([ [.5,0,0],[0,0,0],[0,0,1]])
+Kl = np.matrix([ [10,0,0],[0,-10,0],[0,0,0]])
 controller = GravityCompensationController.GravityCompensationController(Kl,Kv)
 u = [0.0,0.0,0.0]
 while(1):
     count +=1
-    pidConstants = [0.001, 0.0005, .01, 0.002, 0.00025, 0.01, 0.002, 0.0004, 0.01, 0, 0, 0, 0, 0, 0, ];
+
+    pidConstants = [.001, 0.0005, 0.01, 0, 0.00025, 0.01, 0.002, 0.0004, 0.01, 0, 0, 0, 0, 0, 0, ];
     upstream = udp.send_packet(PID_CONFIG, pidConstants)
-    # for k in xrange(sinWaveInc):
-    #     incremtal = float(k / sinWaveInc);
-    #
-    #     for j in xrange(4):
-    #         packet[(j * 3) + 0] = (math.sin(incremtal * math.pi * 2.0) * sin_range) + (sin_range);
-    #         packet[(j * 3) + 1] = 0
-    #         packet[(j * 3) + 2] = 3
+
     i = 0
     upstream = udp.send_packet(37, packet)
     robot.update(upstream)
     u = controller.getTorque(robot)
-    print u
-    packet[0] = robot.angle_to_encoder(0)
-    packet[3] = robot.angle_to_encoder(0)
-    packet[6] = robot.angle_to_encoder(0)
+
+    print robot.tau[1]
+    packet[0] = robot.angle_to_encoder(robot.q[0])
+    packet[3] = robot.angle_to_encoder(robot.q[1])
+    packet[6] = robot.angle_to_encoder(robot.q[2])
+
     packet[2] = u[0]
     packet[5] = u[1]
     packet[8] = u[2]
-
-    upstream =  udp.send_packet(37,packet)
-    robot.update(upstream)
-    print robot.tau
     ploter.update(*Dynamics.fk(robot))
     #print robot.q
 
