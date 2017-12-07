31daa288732246d3e31715f36fbdd083a640fc52
ngoldfarb <nagoldfarb@wpi.edu>
2017-12-07 18:23:55 -0500

    intergrated game

--- complainceTest.py
+++ complainceTest.py
@@ -7,8 +7,11 @@ from Haptic_Controller import GravityCompensationController
 import time
 import numpy as np
 import time
+from Pong_Game import pong
+
 robot = Robot.Robot("arm1")
-ploter = PlotArm.PlotArm()
+#ploter = PlotArm.PlotArm()
+game = pong.pong()
 
 VEL_CONTROL = 48
 VEL_TARGET = 42
@@ -21,11 +24,13 @@ packet = 15*[0,0,0]
 sinWaveInc = 10;
 sin_range = 400;
 count = 0
-Kv = np.matrix([ [.5,0,0],[0,-.5,0],[0,0,-.5]])
-Kl = np.matrix([ [10,0,0],[0,-10,0],[0,0,-10]])
+Kv = np.matrix([ [.5,0,0],[0,.5,0],[0,0,-.5]])
+Kl = np.matrix([ [10,0,0],[0,10,0],[0,0,-10]])
 controller = GravityCompensationController.GravityCompensationController(Kl,Kv)
 u = [0.0,0.0,0.0]
-pidConstants = [.001, 0.0005, 0.01, 0, 0.00025, 0.01, 0.002, 0.0004, 0.01, 0, 0, 0, 0, 0, 0];
+pidConstants = [.001, 0.0005, 0.01, 0, 0.00025, 0.01, 0.002, 0.0004, 0.01, 0, 0, 0, 0, 0, 0]
+
+
 upstream = udp.send_packet(PID_CONFIG, pidConstants)
 while(1):
     count +=1
@@ -40,12 +45,13 @@ while(1):
     packet[2] = u[0]
     packet[5] = u[1]
     packet[8] = u[2]
-    packet[9] = 0
+    hit = game.update(robot)
 
+    packet[9] = hit
     upstream = udp.send_packet(37, packet)
 
     #upstream = udp.send_packet(37, packet)
-    ploter.update(*Dynamics.fk(robot))
+    #ploter.update(*Dynamics.fk(robot))
     #print robot.q
 
 
