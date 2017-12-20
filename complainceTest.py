3a7f700cc746c83d6408014b7c3a5eb017e9c3bb
ngoldfarb <nagoldfarb@wpi.edu>
2017-12-20 11:22:59 -0500

    the complaice test is attempting gravity comp The gametest is trying to do move based on inputs torques

--- complainceTest.py
+++ complainceTest.py
@@ -1,61 +1,73 @@
-
 from UDP.UDP import UDP
 import math
 import Robot
 import PlotArm
 from Dynamics import Dynamics
-from Haptic_Controller import GravityCompensationController
-import time
 import numpy as np
+from Haptic_Controller import GravityCompensationController
+
 import time
-from Pong_Game import pong
 
 robot = Robot.Robot("arm1")
-# ploter = PlotArm.PlotArm()
-game = pong.pong()
+ploter = PlotArm.PlotArm()
 
 VEL_CONTROL = 48
 VEL_TARGET = 42
 PID_CONTROL = 37
 PID_CONFIG = 65
+STATUS = 38
+TORQUE_CONTROL = 39
 
 udp = UDP(9876)
+baseConstants = [0.001, 0.0002, 0.01];
+shoulderConstants = [0.002, 0.00025, 0.01];
+elbowConstants = [0.002, 0.0004, 0.01];
 
 packet = 15 * [0, 0, 0]
-
-Kv = np.matrix([[5, 0, 0], [0, -5, 0], [0, 0, -10]])
-Kl = np.matrix([[25, 0, 0], [0, -10, 0], [0, 0, -50]])
+sinWaveInc = 10;
+sin_range = 400;
+count = 0
+pidConstants = [0.001, 0.0005, .01, 0.002, 0.00025, 0.01, 0.002, 0.0004, 0.01, 0, 0, 0, 0, 0, 0];
+udp.send_packet(PID_CONFIG, pidConstants)
+Kv = np.matrix([[.5, 0, 0], [0, -5, 0], [0, 0, -1]])
+Kl = np.matrix([[1, 0, 0], [0, -10, 0], [0, 0, -50]])
 controller = GravityCompensationController.GravityCompensationController(Kl, Kv)
-u = [0.0, 0.0, 0.0]
-pidConstants = [.0001, 0.0005, 0.001, 0.0001, 0.00025, 0.01, 0.0002, 0.0004, 0.01, 0, 0, 0, 0, 0, 0]
-upstream = udp.send_packet(PID_CONFIG, pidConstants)
-time0 = time.clock()
+
+
+def remap(val, OldMin, OldMax, ):
+    NewMin = 0
+    NewMax = 0.25
+    OldRange = (OldMax - OldMin)
+    NewRange = (NewMax - NewMin)
+    NewValue = (((val - OldMin) * NewRange) / OldRange) + NewMin
+    return abs(round(NewValue, 2))
+
+
 while (1):
+    count += 1
 
-
-
-    i = 0
-    robot.update(upstream)
     u = controller.getTorque(robot)
-    dt = time.clock() - time0
-    packet[0] = robot.angle_to_encoder(robot.qd[0]*dt + robot.q[0])
-    packet[3] = robot.angle_to_encoder(robot.qd[1]*dt + robot.q[1])
-    packet[6] = robot.angle_to_encoder(robot.qd[2]*dt + robot.q[2] + 0.5 * math.pi)
+    #print "link 1: " + str(u[1])
+    #print "link 1: " + str(u[2])
+    packet[0] = robot.angle_to_encoder(0)
+    packet[3] = robot.angle_to_encoder(0)
+    packet[6] = robot.angle_to_encoder(0.5*3.14)
 
+    packet[2] = 0
 
+    packet[5] = remap(0.5*u[1], 0.001, 0.97)
+    packet[8] = remap(0.25*u[2], 0.001, 0.45)
+    packet[9] = 0
+    print "link 1 remap: " + str(packet[5])
+    print "link 2 remap: " + str(packet[8])
 
-
-    packet[2] = u[0]
-    packet[5] = u[1]
-    packet[8] = u[2]
-    pos0, pos1, pos2 = Dynamics.fk(robot)
-    packet[9] = game.update(pos2[1])
-    time0 = time.clock()
-    upstream = udp.send_packet(37, packet)
-
-    # upstream = udp.send_packet(37, packet)
-    # ploter.update(*Dynamics.fk(robot))
+    upstream = udp.send_packet(TORQUE_CONTROL, packet)
+    print upstream
+    robot.update(upstream)
+    # print robot.tau
+    ploter.update(*Dynamics.fk(robot))
     # print robot.q
+
 
 
 
