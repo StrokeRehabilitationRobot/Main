91b99472fcb8d4686600711358d56e28285fc8d7
ngoldfarb <nagoldfarb@wpi.edu>
2017-12-20 22:23:31 -0500

    added comments and removed unessary funtions. Defult params where added to constructor

--- Robot.py
+++ Robot.py
@@ -1,19 +1,27 @@
 
 import numpy as np
 from math import pi
+
+
 class Robot(object):
-    # TODO add dosctrings to functions
-    def __init__(self,arm):
-        if arm == "arm1":
+    """
+    Robot class to hold state and description of the robot
+    """
+    def __init__(self,arm="thanos",id=0):
+        """
+
+        :param arm: which arm do you want
+        :param id: id of the arm
+        """
+        if arm == "thanos":
             self.arm1()
+
         else:
             self.arm2()
-        self.q = [0,0,0]
-        self.qd = [ 0,0,0]
-        self.qdd = [0, 0, 0]
-        self.tau = [0,0,0]
 
-    def arm1(self):
+        self._id = id
+
+
         self._inertia = [[0.006757, 0.0006036, 0.0015514],
                          [0.001745, 0.0005596, 0.00006455],
                          [0.00706657, 0.0006254, 0.0015708]
@@ -21,27 +29,36 @@ class Robot(object):
         self._mass = [1.01992, 0.3519, 0.22772]
         self._lengths = [0.25107, 0.191, 0.37843]
         self._centroid = [0.10424, 0.14550, 0.203]
-        self._convert_factor = ((1 / 11.44) * (2 * pi / 360))
+        self._convert_factor = ((1 / 11.44) * (2 * pi / 360)) # converts from tick->rads
+
+        self.q = [0,0,0]
+        self.qd = [ 0,0,0]
+        self.qdd = [0, 0, 0]
+        self.tau = [0,0,0]
+
+    def arm1(self):
+        """
+        params for arm thanos
+        :return:
+        """
         self._torque_offset = [0.5646, 0.5084, 0.5128]
         self._max_tau = [0.6, 0.55, 0.55]
         self._min_tau = [.45, .40, .4]
 
-    def arm2(self): pass
+    def arm2(self):
 
-    def update_q(self,q):
-        self.q = q
+        self._torque_offset = [0.5646, 0.5084, 0.5128]
+        self._max_tau = [0.6, 0.55, 0.55]
+        self._min_tau = [.45, .40, .4]
 
-    def update_qd(self,qd):
-        self.qd = qd
-
-    def update_qdd(self,qdd):
-        self.qdd = qdd
-
-    def update_tau(self, tau):
-        tau = round(tau,1)
 
     def update(self, state):
         # TODO figure out how to get qdd
+        """
+        updates the state of the robot
+        :param state: the message from the board
+        :return:
+        """
 
         for i in xrange(3):
             self.q[i ] = self.encoder_to_angle(state[i * 3 + 0 + 1])
@@ -50,27 +67,43 @@ class Robot(object):
 
         self.q[2] -= 0.5 * pi
 
-        pass
     def encoder_to_angle(self,ticks):
-        # TODO create function to convert the tick to angles
+        """
+
+        :param ticks: encoder ticks
+        :return: angle of servo
+        """
         return ticks * self._convert_factor
         pass
 
-    # TODO figure out what to do with the load cells to get real data
 
     def angle_to_encoder(self,angle):
-        # TODO create function to convert the tick to angles
+        """
+
+        :param angle: angle of servo
+        :return: encoder ticks
+        """
         return angle / self._convert_factor
-        pass
 
     def unpack(self):
+        """
+
+        :return: all the phyical params of the robot
+        """
         return self._inertia, self._mass, self._lengths, self._centroid
 
     def interpolate_tau(self,updated_tau,i):
+        # TODO figure out scaling for torque for each link
+        """
+
+        :param updated_tau:
+        :param i:
+        :return:
+        """
         return updated_tau#10*updated_tau - 10*self._torque_offset[i]
 
     def filter_tau(self,interpolated_tau,i):
-
+        # TODO figure out analog way of interpring torque
         if interpolated_tau > self._max_tau[i]:
             return 1
         elif interpolated_tau < self._min_tau[i]:
