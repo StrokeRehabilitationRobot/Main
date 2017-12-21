a94ea854510fcd9a18569dc3d0545973221ca6c9
ngoldfarb <nagoldfarb@wpi.edu>
2017-12-20 23:01:00 -0500

    added function to make a packet out of joint info

--- helper.py
+++ helper.py
@@ -18,3 +18,18 @@ def angle_to_encoder(angle):
     """
     convert_factor = ((1 / 11.44) * (2 * pi / 360))  # converts from tick->rads
     return angle / convert_factor
+
+def make_packet(q,qd,tau):
+    """
+
+    :param q: joint values
+    :param qd: joint vels values
+    :param tau: torque vals
+    :return:
+    """
+    packet = 15*[0.0]
+
+    for i in xrange(3):
+        packet[i] = angle_to_encoder(q[i])
+        packet[i+1] = angle_to_encoder(qd[i])
+        packet[i+2] = tau[i]
