3314904df697481e68b880069db6e1ee3a4e0ec2
ngoldfarb <nagoldfarb@wpi.edu>
2017-12-22 09:48:38 -0500

    intergrated helper modulual, seems to work, needs to be tested with haptics in the 2nd two links

--- gameTest.py
+++ gameTest.py
@@ -12,17 +12,10 @@ from Pong_Game import pong
 import helper
 
 
-def remap(val, OldMin, OldMax, ):
-    NewMin = 0
-    NewMax = 0.25
-    OldRange = (OldMax - OldMin)
-    NewRange = (NewMax - NewMin)
-    NewValue = (((val - OldMin) * NewRange) / OldRange) + NewMin
-    return abs(round(NewValue, 2))
-
 
 
 robot = Robot.Robot("arm1")
+udp = UDP(9876)
 # ploter = PlotArm.PlotArm()
 game = pong.pong()
 
@@ -30,8 +23,9 @@ VEL_CONTROL = 48
 VEL_TARGET = 42
 PID_CONTROL = 37
 PID_CONFIG = 65
+STATUS = 38
+TORQUE_CONTROL = 39
 
-udp = UDP(9876)
 
 packet = 15 * [0, 0, 0]
 
@@ -39,7 +33,7 @@ Kv = np.matrix([[5, 0, 0], [0, -5, 0], [0, 0, -10]])
 Kl = np.matrix([[25, 0, 0], [0, -10, 0], [0, 0, -50]])
 controller = CompensationController.CompensationController(Kl, Kv)
 pidConstants = [.0001, 0.0005, 0.001, 0.0001, 0.00025, 0.01, 0.0002, 0.0004, 0.01, 0, 0, 0, 0, 0, 0]
-upstream = udp.send_packet(0,PID_CONFIG, pidConstants)
+upstream = udp.send_packet(0,TORQUE_CONTROL, pidConstants)
 time0 = time.clock()
 
 
@@ -47,14 +41,14 @@ while (1):
 
     u = [0.0, 0.0, 0.0]
     robot.update(upstream)
-    u = controller.getTorque(robot)
+    u = helper.remap(controller.getTorque(robot))
     dt = time.clock() - time0
     packet[0] = helper.angle_to_encoder(robot.qd[0]*dt + robot.q[0])
     packet[3] = helper.angle_to_encoder(robot.qd[1]*dt + robot.q[1])
     packet[6] = helper.angle_to_encoder(robot.qd[2]*dt + robot.q[2] + 0.5 * math.pi)
     packet[2] = u[0]
-    packet[5] = remap(0.5*u[1], 0.001, 0.97)
-    packet[8] = remap(0.1*u[2], 0.001, 0.45)
+    packet[5] = u[1]
+    packet[8] = u[2]
     pos0, pos1, pos2 = Dynamics.fk(robot)
     packet[9] = game.update(pos2[1])
     time0 = time.clock()
